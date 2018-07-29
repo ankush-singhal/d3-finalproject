ae5fefc6e16e88fb9e3dbdec35e6c5ac12eb297d
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-29 18:05:05 -0500

    postion tooltip

--- js/Top10City.js
+++ js/Top10City.js
@@ -62,7 +62,7 @@ d3.csv("/d3-finalproject/data/top10cities.csv", function(d, i, columns) {
   	.on("mousemove", function(d) {
 		tooltip1
 		.style("left", d3.event.pageX - 500 + "px")
-         	.style("top", d3.event.pageY - 100 + "px")
+         	.style("top", d3.event.pageY - 75 + "px")
 	  	.style("display", "inline-block")
 		.html("City Name: <b>"+d.data.city+"</b>"+ "<br>" + "Number of Employees: " +"<b>"+(d[1]-d[0]));    			
 		})
