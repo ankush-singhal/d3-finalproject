def18d1d944e48b7b2e684d78d1d2ac0b11f8552
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-29 02:39:39 -0500

    test6

--- js/Top10City.js
+++ js/Top10City.js
@@ -1,13 +1,13 @@
-var margin1 = {top: 10, right: 50, bottom: 0, left: 250};
+var margin1 = {top: 10, right: 50, bottom: 5, left: 250};
 
 var svg1 = d3.select("div#vis1")
   .append("svg")
   .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "0 0 960 460")
+  .attr("viewBox", "0 0 960 480")
   .classed("svg-content", true);
 
  var width1 = 960 - margin1.left - margin1.right;
- var height1 = 460 - margin1.top - margin1.bottom;
+ var height1 = 480 - margin1.top - margin1.bottom;
   
 var g1 = svg1.append("g")
   .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
